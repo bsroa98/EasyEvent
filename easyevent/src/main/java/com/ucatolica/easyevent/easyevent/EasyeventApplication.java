b7d8c7f0f6760924cf4b3efc75ca1cc13ce39b6a
bsroa98 <bsroa98@ucatolica.edu.co>
2023-09-20 21:39:32 -0500

    Commit Proyecto Base.

--- easyevent/src/main/java/com/ucatolica/easyevent/easyevent/EasyeventApplication.java
+++ easyevent/src/main/java/com/ucatolica/easyevent/easyevent/EasyeventApplication.java
@@ -4,12 +4,14 @@ import com.ucatolica.easyevent.easyevent.persitencia.ProveedorCrudRepository;
 import com.ucatolica.easyevent.easyevent.persitencia.ProveedorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import com.ucatolica.easyevent.easyevent.persitencia.ProveedorCrudRepository;
 @SpringBootApplication
-public class EasyeventApplication {
+public abstract class EasyeventApplication implements ProveedorCrudRepository{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyeventApplication.class, args);
+
+
 	}
 
 }
