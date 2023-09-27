d109dd2f1da2d78f14ab4d7f04809f01d7197cbf
bsroa98 <bsroa98@ucatolica.edu.co>
2023-09-27 18:39:29 -0500

    Revert "Proceso registrar"
    This reverts commit dcbb03b31f84595c6d5e554b78f7d8922ed9ac50.

---
+++ easyevent/src/main/java/com/ucatolica/easyevent/easyevent/registrar/ResgistrarUsuario.java
@@ -0,0 +1,15 @@
+package com.ucatolica.easyevent.easyevent.registrar;
+
+class ResgistrarUsuario {
+    private int id;
+    private String nombre;
+    private String email;
+
+    public RegistrarUsuario(int id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+
