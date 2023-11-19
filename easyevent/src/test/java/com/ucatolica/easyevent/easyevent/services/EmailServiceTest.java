6922fdb79114081e6954780561295a594960a1d8
bsroa98 <bsroa98@ucatolica.edu.co>
2023-11-18 23:09:50 -0500

    Initial Test

---
+++ easyevent/src/test/java/com/ucatolica/easyevent/easyevent/services/EmailServiceTest.java
@@ -0,0 +1,20 @@
+package com.ucatolica.easyevent.easyevent.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void sendEmail() {
+
+
+    }
+}
